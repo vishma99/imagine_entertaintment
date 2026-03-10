abe149d4fcecbd17d5d48cd22fb1ac6c963348d0
vishma99 <vishmagunawardhnama99@gmail.com>
2026-03-10 23:55:01 +0530

    new update

--- imagine-Backend/config/mailer.js
+++ imagine-Backend/config/mailer.js
@@ -1,8 +1,5 @@
 import nodemailer from "nodemailer";
-import dotenv from "dotenv";
 import dns from "dns";
-
-dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
@@ -12,13 +9,13 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  // Render සඳහා මෙය අනිවාර්යයි!
   lookup: (hostname, options, callback) => {
     dns.lookup(hostname, { family: 4 }, callback);
   },
   tls: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
-// මෙය අනිවාර්යයි!
 export default transporter;
